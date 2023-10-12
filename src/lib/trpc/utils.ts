bfbfa48c0269174f18049d7deca9c5a170925a83
CoppoloCode <matthewcoppolo@yahoo.com>
2023-10-12 15:15:44 -0400

    test

--- src/lib/trpc/utils.ts
+++ src/lib/trpc/utils.ts
@@ -1,7 +1,8 @@
 import { env } from "@/lib/env.mjs";
 function getBaseUrl() {
+  console.log(env.VERCEL_URL)
   if (typeof window !== "undefined") return "";
-  if (env.VERCEL_URL) return `${env.VERCEL_URL}`;
+  if (env.VERCEL_URL) return `https://${env.VERCEL_URL}`;
   return "http://localhost:3000";
 }
 
